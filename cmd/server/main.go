6a296768c576ec401f7e750e2ec7371e574389ed
alexare8 <duncorr@gmail.com>
2026-08-21 18:10:49 -0400

    CH6L3 Server subscribe to game_log queue and writes log to file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,17 +25,17 @@ func main() {
 		log.Fatalf("could not create channel: %v", err)
 	}
 
-	_, queue, err := pubsub.DeclareAndBind(
+	err = pubsub.SubscribeGob(
 		conn,                       // connection
 		routing.ExchangePerilTopic, // exchange
 		routing.GameLogSlug,        // queue name
 		routing.GameLogSlug+".*",   // routing key
 		pubsub.SimpleQueueDurable,  // queue type
+		handlerLogs(),              // handler
 	)
 	if err != nil {
-		log.Fatalf("could not subscribe to game_logs: %v", err)
+		log.Fatalf("could not start consuming logs: %v", err)
 	}
-	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
 
 	gamelogic.PrintServerHelp()
 
